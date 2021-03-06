f87844d5e6c410cd30402a49bbb29e7ab9b39a0f
mehdi <mehdi-bs@live.fr>
2021-03-06 19:34:38 +0100

    new structure project

---
+++ src/lib.rs
@@ -0,0 +1,90 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+
+
+pub fn compute_route_cost(route: &Vec<i32>, distances: &Vec<Vec<f64>>) -> f64 {
+    // Computes the cost (length) of route according to distances matrix
+    let mut cost: f64 = 0.0;
+    for i in 1..route.len() {
+        cost += distances[route[i-1] as usize][route[i] as usize];
+    }
+    return cost;
+}
+
+#[pyfunction]
+fn solver_2opt(route: Vec<i32>, distances: Vec<Vec<f64>>, symmetric: bool) -> PyResult<Vec<i32>> {
+    // Implement 2-opt local search starting from route
+    let mut best: Vec<i32> = route.clone(); // best sol
+    let mut compbest: Vec<i32> = route.clone(); // best sol companion
+    let mut improved: bool = true; // monitor improvement
+    let mut cost_change: f64 = 0.0; // change in cost at each iteration
+    let mut best_cost: f64 = compute_route_cost(&route, &distances);
+    let mut candidate_cost: f64;
+    while improved {
+        improved = false;
+        for i in 1..route.len() - 2 {
+            for j in i+1..route.len(){
+                if j - i == 1 {
+                    continue;
+                }
+                if symmetric {
+                    cost_change = distances[best[i-1] as usize][best[j-1] as usize] + distances[best[i] as usize][best[j] as usize] - distances[best[i-1] as usize][best[i] as usize] - distances[best[j-1] as usize][best[j] as usize];
+                    if cost_change < 0.0 {
+                        for k in 0..j-i {
+                            best[i+k as usize] = compbest[j-1-k as usize];
+                        }
+                        improved = true;
+                        compbest.clone_from_slice(&best);
+                    }
+                }
+                else {
+                    for k in 0..j-i {
+                        compbest[i+k as usize] = best[j-1-k as usize];
+                    }
+                    candidate_cost = compute_route_cost(&compbest, &distances);
+                    if candidate_cost < best_cost {
+                        best_cost = candidate_cost;
+                        best.clone_from_slice(&compbest);
+                        improved = true;
+                    }
+                    else {
+                        compbest.clone_from_slice(&best);
+                    }
+
+                }
+            }
+        }
+        
+    }
+    Ok(best.to_vec()) 
+}
+
+#[pyfunction]
+fn solver_2opt_parr(py: Python, route: Vec<i32>, distances: Vec<Vec<f64>>, symmetric: bool) -> PyResult<Vec<i32>>  {
+    py.allow_threads(|| solver_2opt(route, distances, symmetric))
+}
+
+
+
+#[pymodule]
+fn rust_2opt(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(solver_2opt))?;
+    m.add_wrapped(wrap_pyfunction!(solver_2opt_parr))?;
+    Ok(())
+}
+
+
+// Some tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn cost_computation() {
+        let route: Vec<i32> = vec![0, 1, 2, 0];
+        let distances: Vec<Vec<f64>> = vec![vec![0.0, 2.0, 3.0], vec![2.0, 0.0, 4.0], vec![3.0, 4.0, 0.0]];
+        let dist = compute_route_cost(&route, &distances);
+        assert_eq!(dist, 9.0);
+    }
+}
+
---
+++ tsp_2opt/solver.py
@@ -0,0 +1,32 @@
+from concurrent.futures import ThreadPoolExecutor
+from rust_2opt import solver_2opt_parr
+from utils import get_best_from_batch, get_init_route, is_symmetric, check_type
+import numpy as np 
+import time 
+
+
+def tsp_solver(distances: np.ndarray, workers: int = 5, r: int = 10):
+    """ 
+    """
+    #-- Check input type
+    distances = check_type(distances)
+    #-- Check symmetry
+    symmetric = is_symmetric(distances)
+    executor = ThreadPoolExecutor(max_workers=workers)
+    nodes = len(distances)
+    solutions = [None] * r
+    #--
+    for i in range(r):
+        #-- Seed for reproducibility
+        init_route = get_init_route(nodes, seed=i)
+        future = executor.submit(
+            solver_2opt_parr, init_route, distances, symmetric
+        )
+        solutions[i] = future
+    #--
+    for i in range(r):
+        solutions[i] = solutions[i].result()
+    #-- Retrieve best solution
+    best, length = get_best_from_batch(solutions, distances)
+    return best, length
+    
